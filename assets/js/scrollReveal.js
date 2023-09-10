f4e23734eb9499b75eb2ad627f8d76ed7e41e6ea
Isaac Finotti <imisaac2807@gmail.com>
2023-09-10 19:14:52 -0300

    Footer

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,13 +1,13 @@
 const scrollReveal = ScrollReveal({
   origin: "top",
   distance: "30px",
-  duration: 700, //mili segundos
+  duration: 500, //ms
   reset: true,
 });
 
 scrollReveal.reveal(
   `.gray-section, .black-section, .section, .grid, .text-content`,
   {
-    interval: 100,
+    interval: 10,
   },
 );
